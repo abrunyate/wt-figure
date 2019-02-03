2444b3cd11d659cfc71951e87d870ec113cc04f3
Adrian Brunyate <brunyate@gmail.com>
2019-02-03 12:03:19 -0500

    Add in template machinery that was previously in web site.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This is a simple extension to look up unresolved reference style image links
 in a markdown document in a modelcluster and render them as figures.
+
+This code is slowly being factored out of my web site. In particular,
+don't expect it to be good or even usable yet.
---
+++ wtfigure/templatetags/markdown_figures.py
@@ -0,0 +1,24 @@
+from django import template
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+import re
+
+register = template.Library()
+
+#TODO: Accept single quotes.
+tag_cre=re.compile(r'\{%\s*figure\s+(\S*)\s+("[^"]*")\s*%}')
+
+@register.filter
+def link_figures(value, arg):
+  def subs_fun(match):
+    smart_image = arg.images.get(handle=match.group(1)).image
+    print(smart_image)
+    return render_to_string("figure.html", {"smart_image": smart_image})
+
+  return mark_safe(tag_cre.sub(subs_fun, value))
+
+
+@register.filter
+def link_md(value, arg):
+  return value+'\n\n'+arg.links()
