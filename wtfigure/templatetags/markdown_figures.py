7401704fb5a33f49c54767b7d17676316563f6f4
Adrian Brunyate <brunyate@gmail.com>
2019-02-11 16:02:33 -0500

    Add alt text to template context.

--- wtfigure/templatetags/markdown_figures.py
+++ wtfigure/templatetags/markdown_figures.py
@@ -7,13 +7,15 @@ import re
 register = template.Library()
 
 #TODO: Accept single quotes.
-tag_cre=re.compile(r'\{%\s*figure\s+(\S*)\s+("[^"]*")\s*%}')
+#Groups: 0--whole tag. 1--smart_image. 2--alt text
+tag_cre=re.compile(r'\{%\s*figure\s+(\S*)\s+"([^"]*)"\s*%}')
 
 @register.filter
 def link_figures(value, arg):
   def subs_fun(match):
     smart_image = arg.images().get(handle=match.group(1)).image
-    return render_to_string("figure.html", {"smart_image": smart_image})
+    return render_to_string("figure.html", {"smart_image": smart_image,
+                                            "alt": match.group(2)})
 
   return mark_safe(tag_cre.sub(subs_fun, value))
 
